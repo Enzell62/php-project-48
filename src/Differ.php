70786780df18443a26ba47f558e5298e0f1cbd4e
Enzell62 <enzell62@gmail.com>
2025-08-21 13:09:24 +0500

    before rework Diff for recursive

--- src/Differ.php
+++ src/Differ.php
@@ -27,7 +27,7 @@ class Differ
     {
         $result = [];
         $uniquesFile1 = array_diff_key($file1, $file2);
-        foreach ($uniquesFile1 as $key => $value) {
+        foreach ($uniquesFile1 as $key => $value) { // с рекурсией ломается, потому что array_diff_key сравнивает не только ключи но и значения
             if (is_array($value)) { // для рекурсии запускаем сравнение значения самим с собой, чтобы разметить
                 $value = self::compare($value, $value);
             } else {
@@ -47,7 +47,7 @@ class Differ
             if (is_array($file1[$intersectKey]) && is_array($file2[$intersectKey])) { // рекурсия
                 $result[] = self::compare($file1[$intersectKey], $file2[$intersectKey]);
             } // дописать рекурсию для вариантов, когда одно значение массив, а другое нет
-            if ($file1[$intersectKey] == $file2[$intersectKey]) {
+            if ($file1[$intersectKey] == $file2[$intersectKey]) { // поставить тут строгое сравнение ?
                 $result[] = self::createItem($intersectKey, $file1[$intersectKey], null, 'both');
             } else {
                 $result[] = self::createItem($intersectKey, $file1[$intersectKey], 'file1', 'modified');
--- test.php
+++ test.php
@@ -11,11 +11,14 @@ $path2 = 'input/file2.json';
 $path4 = 'input/file4_YAML.yaml';
 $path5 = 'input/file5.yml';
 $path6 = 'input/file6.yaml';
-$path7 = 'tests/fixtures/fixturesFormatter1.json';
+$path7 = 'input/file7.json';
+$path8 = 'input/file8.json';
+$path9 = 'tests/fixtures/fixturesFormatter1.json';
 
-$file1 = Parser::parse($path5);
-$file2 = Parser::parse($path6);
+
+$file1 = Parser::parse($path7);
+$file2 = Parser::parse($path8);
 
 $dif = Differ::compare($file1, $file2);
 $formatted = Formatter::format($dif);
-print_r($formatted);
+print_r($dif);
