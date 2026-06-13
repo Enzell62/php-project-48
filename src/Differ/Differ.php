c62b3eb79c0c14e46acb663b79038572b2ae2bb1
Enzell62 <enzell62@gmail.com>
2026-06-13 19:25:57 +0500

    gemini rework 6

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -4,6 +4,23 @@ namespace Differ\Differ;
 
 use function Funct\Collection\union;
 use function Funct\Collection\sortBy;
+use function Differ\Parser\parse;
+use function Differ\Formatter\format;
+
+function genDiff(string $path1, string $path2, string $formatName = 'stylish'): string
+{
+    $data1 = file_get_contents($path1);
+    $type1 = pathinfo($path1, PATHINFO_EXTENSION);
+    $parsed1 = parse($data1, $type1);
+
+    $data2 = file_get_contents($path2);
+    $type2 = pathinfo($path2, PATHINFO_EXTENSION);
+    $parsed2 = parse($data2, $type2);
+
+    $diffTree = genDiffTree($parsed1, $parsed2);
+
+    return format($diffTree, $formatName);
+}
 
 function genDiffTree(array $data1, array $data2): array
 {
